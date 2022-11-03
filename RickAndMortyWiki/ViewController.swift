b6162fefaab3f6bdf69e74e69a39b97bd2ad7a59
Igor Fernandes <igormedfer@gmail.com>
2022-11-02 23:13:36 -0300

    added collection view

--- RickAndMortyWiki/ViewController.swift
+++ RickAndMortyWiki/ViewController.swift
@@ -8,12 +8,64 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    lazy var collectionView: UICollectionView = {
+        // flow layout
+        let layout = UICollectionViewFlowLayout()
+        layout.itemSize = CGSize(width: view.bounds.width - 10, height: 200)
+        layout.estimatedItemSize = .zero
+        layout.sectionInset = .init(top: 0, left: 0, bottom: 0, right: 0)
+        
+        // create collection
+        let collection = UICollectionView(frame: .zero, collectionViewLayout: layout)
+        collection.backgroundColor = .purple
+        collection.delegate = self
+        collection.dataSource = self
+        collection.translatesAutoresizingMaskIntoConstraints = false
+        collection.register(UICollectionViewCell.self, forCellWithReuseIdentifier: "cell")
+        return collection
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        title = "Welcome"
+        view.backgroundColor = .red
+        setupElements()
     }
+    
+    func setupElements() {
+        collectionViewConstraints()
+    }
+    
+    func collectionViewConstraints() {
+        view.addSubview(collectionView)
+        
+        NSLayoutConstraint.activate([
+            collectionView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 10),
+            collectionView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 5),
+            collectionView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -5),
+            collectionView.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: -5)
+        ])
+    }
+}
 
-
+extension ViewController: UICollectionViewDataSource, UICollectionViewDelegate, UICollectionViewDelegateFlowLayout {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath)
+        cell.backgroundColor = .black
+        cell.layer.borderColor = UIColor.green.cgColor
+        cell.layer.borderWidth = 5
+        cell.layer.cornerRadius = 5
+        cell.clipsToBounds = true
+        return cell
+    }
+    
+//    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+//        return CGSize(width: 100, height: 150)
+//    }
+    
 }
 
